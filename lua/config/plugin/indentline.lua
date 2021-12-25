9db9c5554f9a8162fb39b55ea2fbb04bff7e1a83
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2021-12-25 19:28:45 +0530

    added additional context patterns

--- lua/config/plugin/indentline.lua
+++ lua/config/plugin/indentline.lua
@@ -17,6 +17,30 @@ g.indent_blankline_filetype_exclude = {
    'lspinfo',
    'TelescopePrompt',
    'TelescopeResults',
+   ' '
+}
+vim.g.indent_blankline_context_patterns = {
+	"class",
+	"return",
+	"function",
+	"method",
+	"^if",
+	"^while",
+	"jsx_element",
+	"^for",
+	"^object",
+	"^table",
+	"block",
+	"arguments",
+	"if_statement",
+	"else_clause",
+	"jsx_element",
+	"jsx_self_closing_element",
+	"try_statement",
+    "switch_statement",
+	"catch_clause",
+	"import_statement",
+	"operation_type",
 }
 g.indent_blankline_show_first_indent_level = true
 g.indent_blankline_use_treesitter = true
