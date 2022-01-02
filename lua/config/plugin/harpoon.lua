0f7b09c36bc45a92524fbb515a8801e4d122e1e7
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-01-02 13:58:07 +0530

    more intuitive keymaps for harpoon

--- lua/config/plugin/harpoon.lua
+++ lua/config/plugin/harpoon.lua
@@ -2,8 +2,8 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = false }
 
 map('n', '<leader>hm', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', opts)
-map('n', '<leader>ha', '<cmd>lua require("harpoon.mark").add_file()<cr>', opts)
-map('n', '<leader>h1', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>', opts)
-map('n', '<leader>h2', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>', opts)
-map('n', '<leader>h3', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>', opts)
-map('n', '<leader>h4', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>', opts)
+map('n', '<leader>m', '<cmd>lua require("harpoon.mark").add_file()<cr>', opts)
+map('n', '<leader>ma', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>', opts)
+map('n', '<leader>ms', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>', opts)
+map('n', '<leader>md', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>', opts)
+map('n', '<leader>mf', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>', opts)
