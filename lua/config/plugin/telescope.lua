33dfada713fe0f76b9f2037b011bc9246f7987df
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-01-02 13:59:29 +0530

    load harpoon extension for telescope only if available

--- lua/config/plugin/telescope.lua
+++ lua/config/plugin/telescope.lua
@@ -7,7 +7,6 @@ require('telescope').setup {
     }
 }
 require('telescope').load_extension('fzy_native')
-
 -- keymaps
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
@@ -16,3 +15,12 @@ map('n', '<C-f>', '<cmd>lua require("telescope.builtin").find_files()<cr>', opts
 map('n', '<C-b>', '<cmd>lua require("telescope.builtin").buffers()<cr>', opts)
 map('n', '<leader>th', '<cmd>lua require("telescope.builtin").help_tags()<cr>', opts)
 map('n', '<leader>tg', '<cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
+
+local status_ok, harpoon = pcall(require, "harpoon")
+if status_ok then
+    require("telescope").load_extension('harpoon')
+    map('n', '<leader>tm', '<cmd>Telescope harpoon marks<cr>', opts)
+else
+    return
+end
+
