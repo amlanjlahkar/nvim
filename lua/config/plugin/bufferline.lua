4c38f36927191f3ede1dc342423d9d9e9e0dbd57
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-01-27 19:59:51 +0530

    adjst(file_format): adjust indention

--- lua/config/plugin/bufferline.lua
+++ lua/config/plugin/bufferline.lua
@@ -17,55 +17,43 @@ bufferline.setup {
         numbers = function(opts)
             return string.format('%s. ', opts.ordinal)
         end
-	},
-	highlights = {
-		fill = {
-			guifg = {attribute = "fg", highlight = "Normal"},
-			guibg = {attribute = "bg", highlight = "StatusLineNC"},
-		},
-		background = {
-			guifg = {attribute = "fg", highlight = "Normal"},
-			guibg = {attribute = "bg", highlight = "StatusLine"}
-		},
-		buffer_visible = {
-			gui = "",
+    },
+    highlights = {
+        fill = {
+            guifg = {attribute = "fg", highlight = "Normal"},
+            guibg = {attribute = "bg", highlight = "StatusLineNC"},
+        },
+        background = {
+            guifg = {attribute = "fg", highlight = "Normal"},
+            guibg = {attribute = "bg", highlight = "StatusLine"}
+        },
+        buffer_visible = {
+            gui = "",
             guifg = {attribute = "fg", highlight="Normal"},
             guibg = {attribute = "bg", highlight = "Normal"}
-		},
-		buffer_selected = {
-			gui = "Bold",
+        },
+        buffer_selected = {
+            gui = "",
             guifg = {attribute = "fg", highlight="Normal"},
             guibg = {attribute = "bg", highlight = "Normal"}
-		},
+        },
         modified = {
             guifg = {attribute = "fg", highlight="Character"},
             guibg = {attribute = "bg", highlight = "StatusLine"}
         },
-		separator = {
-			guifg = {attribute = "bg", highlight = "Normal"},
-			guibg = {attribute = "bg", highlight = "StatusLine"},
-		},
-		separator_selected = {
+        separator = {
+            guifg = {attribute = "bg", highlight = "Normal"},
+            guibg = {attribute = "bg", highlight = "StatusLine"},
+        },
+        separator_selected = {
             guifg = {attribute = "fg", highlight="Special"},
             guibg = {attribute = "bg", highlight = "Normal"}
-		},
-		separator_visible = {
-			guifg = {attribute = "fg", highlight = "Normal"},
-			guibg = {attribute = "bg", highlight = "StatusLineNC"},
-		},
-		close_button = {
-			guifg = {attribute = "fg", highlight = "Normal"},
-			guibg = {attribute = "bg", highlight = "StatusLine"}
-		},
-		close_button_selected = {
-            guifg = {attribute = "fg", highlight="normal"},
-            guibg = {attribute = "bg", highlight = "normal"}
-		},
-		close_button_visible = {
-            guifg = {attribute = "fg", highlight="normal"},
-            guibg = {attribute = "bg", highlight = "normal"}
-		}
-	}
+        },
+        separator_visible = {
+            guifg = {attribute = "fg", highlight = "Normal"},
+            guibg = {attribute = "bg", highlight = "StatusLineNC"},
+        }
+    }
 }
 
 -- keymap
