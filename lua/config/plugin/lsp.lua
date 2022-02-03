62abaf18bf93a969c245d6b14a4277b6f88eed71
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-02-03 18:07:25 +0530

    style(lsp): change signcolumn icons

--- lua/config/plugin/lsp.lua
+++ lua/config/plugin/lsp.lua
@@ -68,7 +68,7 @@ end
 
 
 -- use custom diagnostic signs
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
