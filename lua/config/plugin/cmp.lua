fa3750886ef771455ac128d4d5cc665b066eb925
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-02-09 20:42:28 +0530

    feat(snippets): luasnip integration

--- lua/config/plugin/cmp.lua
+++ lua/config/plugin/cmp.lua
@@ -1,86 +1,93 @@
-local cmp_available, cmp = pcall(require, "cmp")
-local snip_available, luasnip = pcall(require, "luasnip")
-if not cmp_available and snip_available then
-    return
+local function prequire(...)
+local status, lib = pcall(require, ...)
+if (status) then return lib end
+    return nil
 end
-
-require('luasnip/loaders/from_vscode').lazy_load()
+local cmp = prequire("cmp")
+local ls = prequire("luasnip")
 
 local kind_icons = {
-  Text = "",
-  Method = "m",
-  Function = "",
-  Constructor = "",
-  Field = "",
-  Variable = "",
-  Class = "",
-  Interface = "",
-  Module = "",
-  Property = "",
-  Unit = "",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
+    Class = "ﴯ ",
+    Color = " ",
+    Constant = "",
+    Constructor = " ",
+    Enum = "練",
+    EnumMember = " ",
+    Event = " ",
+    Field = "識",
+    File = "",
+    Folder = " ",
+    Function = " ",
+    Interface = "ﰮ ",
+    Keyword = " ",
+    Method = " ",
+    Module = " ",
+    Operator = "",
+    Property = " ",
+    Reference = "渚",
+    Snippet = " ",
+    Struct = "פּ ",
+    Text = " ",
+    TypeParameter = " ",
+    Unit = "塞",
+    Value = " ",
+    Variable = "",
 }
 
 cmp.setup({
     snippet = {
         expand = function(args)
-            luasnip.lsp_exapand(args.body)
+            ls.lsp_expand(args.body)
         end,
     },
+
     mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-k>'] = cmp.mapping(cmp.mapping.scroll_docs(-2), { 'i', 'c' }),
-        ['<C-j>'] = cmp.mapping(cmp.mapping.scroll_docs(2), { 'i', 'c' }),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-        ['<C-e>'] = cmp.mapping({
+        -- general cmp mappings
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping(cmp.mapping.scroll_docs(-2), { "i", "s" }),
+        ['<C-j>'] = cmp.mapping(cmp.mapping.scroll_docs(2), { "i", "s" }),
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "s" }),
+        ["<C-e>"] = cmp.mapping({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
         }),
-        --['<CR>'] = cmp.mapping.confirm({
-        --    behavior = cmp.ConfirmBehavior.Replace,
-        --    select = false,
-        --}),
-        ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expandable() then
-                luasnip.expand()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
+        ["<CR>"] = cmp.mapping.confirm({
+           behavior = cmp.ConfirmBehavior.Replace,
+           select = false,
+        }),
+
+        -- luasnip mappings
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if ls.expand_or_locally_jumpable() then
+                ls.expand_or_jump()
             else
                 fallback()
             end
-        end,
-        ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
+        end, { "i", "s", silent = true }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if ls.jumpable(-1) then
+                ls.jump(-1)
             else
                 fallback()
             end
-        end,
+        end, { "i", "s", silent = true }),
+
+        ["<C-l>"] = cmp.mapping(function(fallback)
+            if ls.choice_active() then
+                ls.change_choice()
+            end
+        end, { "i", "s", silent = true }),
     },
+
     sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip'  },
         { name = 'buffer'   },
         { name = 'path'     },
     },
+
     formatting = {
         fields = { "kind", "abbr", "menu" },
         format = function(entry, vim_item)
@@ -94,6 +101,7 @@ cmp.setup({
             return vim_item
         end
     },
+
     get_bufnrs = function()
         local bufs = {}
         for _, win in ipairs(vim.api.nvim_list_wins()) do
@@ -102,8 +110,3 @@ cmp.setup({
         return vim.tbl_keys(bufs)
     end
 })
-
---[[ cmp.setup.cmdline('/', {
-    sources = {{ name = 'buffer' }},
-}) ]]
-
