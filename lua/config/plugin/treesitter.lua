54fd37b2be80478b8a7be7293d9946fe3167f79b
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-02-10 21:40:59 +0530

    adjst(plugin): update conf for TS

--- lua/config/plugin/treesitter.lua
+++ lua/config/plugin/treesitter.lua
@@ -11,6 +11,16 @@ TS_configs.setup {
         disable = {},
         additional_vim_regex_highlighting = false,
     },
+    incremental_selection = {
+        enable = false,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+        },
+    },
 }
+
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.bash.used_by = { "sh", "zsh" }
