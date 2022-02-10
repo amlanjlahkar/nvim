5c9420f81c96715a1f28acf049a1c9ef4b4c240d
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-02-10 21:43:33 +0530

    adjst(init): update

--- init.lua
+++ init.lua
@@ -7,10 +7,12 @@ require('colorscheme')
 -- plugins
 local is_available, _ = pcall(require, "impatient")
 if is_available then
-    require('impatient') -- must be loaded before any lua modules
+    require("impatient") -- must be loaded before any lua modules
 end
 require('packer/plugins')
 local compiled_obj = vim.fn.stdpath('config') .. '/lua/packer/packer_compiled.lua'
-if vim.fn.exists(compiled_obj) then
+if vim.fn.exists(vim.fn.glob(compiled_obj)) then
     require('packer/packer_compiled')
+else
+    require("packer").compile()
 end
