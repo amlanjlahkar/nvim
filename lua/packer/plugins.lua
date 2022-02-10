88291acc505bd2509495e5a39d1545431ca104a8
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-02-10 21:42:48 +0530

    adjst(packer): update

--- lua/packer/plugins.lua
+++ lua/packer/plugins.lua
@@ -1,9 +1,8 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     print('Installing packer...(reopen neovim for the changes to take effect)')
-    vim.cmd "packadd packer.nvim"
 end
 local is_available, packer = pcall(require, "packer")
 if not is_available then
@@ -11,7 +10,7 @@ if not is_available then
 end
 
 -- function to conveniently source plugin configurations
-function use_config(plugin)
+local function use_config(plugin)
     return string.format('require("config/plugin/%s")', plugin)
 end
 
@@ -38,46 +37,44 @@ return packer.startup({function(use)
     -- fuzzy finding
     use {
         'nvim-telescope/telescope.nvim',
-        requires = {{ 'nvim-telescope/telescope-fzy-native.nvim' }},
+        requires = 'nvim-telescope/telescope-fzy-native.nvim',
         config = use_config("telescope")
     }
 
     -- lsp related
     use {
-        'neovim/nvim-lspconfig', as = 'lsp',
-        ft = { 'cpp', 'c', 'objc', 'objcpp' },
-        config = use_config("lsp")
+        'neovim/nvim-lspconfig',
+        ft = { 'cpp', 'c', 'objc', 'objcpp', 'lua' },
+        config = [[ require('config/lsp') ]]
     }
     use {
-        'hrsh7th/nvim-cmp', as = 'cmp',
+        'hrsh7th/nvim-cmp',
         event = { "InsertEnter" },
-        wants = { 'snip' },
+        wants = { "LuaSnip" },
         requires = {
-            { 'hrsh7th/cmp-buffer', after = 'cmp'},
-            { 'hrsh7th/cmp-path' , after = 'cmp' },
-            { 'hrsh7th/cmp-nvim-lsp', after = 'cmp' },
-            { 'saadparwaiz1/cmp_luasnip', after = 'cmp' },
+            { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
+            { 'hrsh7th/cmp-path' , after = 'nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' },
+            { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' },
         },
         config = use_config("cmp")
     }
     use {
-        'L3MON4D3/LuaSnip', as = 'snip',
-        after = 'cmp',
+        'L3MON4D3/LuaSnip',
+        after = "nvim-cmp",
         config = use_config("luasnip")
     }
     use {
         'folke/trouble.nvim',
         requires = 'kyazdani42/nvim-web-devicons',
-        after = 'lsp', 
-        config = function()
-            require('trouble').setup()
-        end 
+        after = "nvim-lspconfig",
+        config = use_config("trouble")
     }
 
     -- treesitter
     use {
         'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', opt = true,
-        ft = { 'bash', 'sh', 'zsh', 'cpp', 'c', 'css', 'html', 'javascript', 'lua', 'python', "yaml" },
+        ft = { 'bash', 'sh', 'zsh', 'cpp', 'c', 'css', 'html', 'javascript', 'lua', 'python', 'vim', 'yaml' },
         config = use_config("treesitter")
     }
 
@@ -89,7 +86,7 @@ return packer.startup({function(use)
     use {
         'windwp/nvim-autopairs',
         config = function()
-            require('nvim-autopairs').setup()
+            require("nvim-autopairs").setup()
         end
     }
     use {
@@ -99,21 +96,21 @@ return packer.startup({function(use)
     use {
         'numToStr/Comment.nvim',
         config = function()
-            require('Comment').setup()
+            require("Comment").setup()
         end
     }
 
     -- colorscheme
     use 'sainnhe/everforest'
 
-    if packer_bootstrap then
+    if PACKER_BOOTSTRAP then
         require("packer").sync()
     end
 end,
     config = {
         display = {
             open_fn = function()
-                return require('packer.util').float({ border = 'rounded' })
+                return require("packer.util").float({ border = "rounded" })
             end
         },
         profile = {
