8cead22a910d3b63e47697cc12a52afe155d1f6c
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-04-15 20:33:43 +0530

    update

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ require('colorscheme')
 -- create cache files for better startuptime
 local is_available, _ = pcall(require, "impatient")
 if not is_available then
-    return
+  require('packer/plugins')
 end
 
 -- plugins
@@ -15,5 +15,5 @@ require('packer/plugins')
 local fn = vim.fn
 local compiled_obj = fn.stdpath('config') .. '/lua/packer/packer_compiled.lua'
 if fn.empty(fn.glob(compiled_obj)) ~= 1 then
-    require('packer/packer_compiled')
+  require('packer/packer_compiled')
 end
