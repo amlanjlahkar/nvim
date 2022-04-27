92dddbe1acadba61ade286ca52c79a9b94abd7d9
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-04-27 19:45:03 +0530

    appearance changes for nvim-lsp and nvim-cmp

--- lua/config/lsp/settings.lua
+++ lua/config/lsp/settings.lua
@@ -16,7 +16,7 @@ local function lsp_ui()
           prefix = "",
         },
     })
-    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    local signs = { Error = "E ", Warn = "W ", Hint = "H ", Info = "I " }
     for type, icon in pairs(signs) do
         local hl = "DiagnosticSign" .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
