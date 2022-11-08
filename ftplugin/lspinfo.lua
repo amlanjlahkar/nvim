92259a1af65fcde3d2114e7d4d1bff5bf46be341
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-11-08 13:22:14 +0530

    style: better organize custom color table and use border in lspinfo window

---
+++ ftplugin/lspinfo.lua
@@ -0,0 +1,1 @@
+vim.opt_local.winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel"
--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -2,6 +2,7 @@ local is_available, _ = pcall(require, "lspconfig")
 if not is_available then
   return
 end
+require("lspconfig.ui.windows").default_options.border = "single"
 
 require("config.lsp.server")
 require("config.lsp.handler").setup()
