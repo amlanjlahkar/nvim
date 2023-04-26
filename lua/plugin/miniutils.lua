a40f33465e81870a7bfbc81c2c147bbc00d0ae2b
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2023-04-26 21:41:30 +0530

    feat(miniutils): add mini.ai

--- lua/plugin/miniutils.lua
+++ lua/plugin/miniutils.lua
@@ -62,4 +62,36 @@ return {
       })
     end,
   },
+
+  {
+    "echasnovski/mini.ai",
+    version = false,
+    keys = { "vi", "va" },
+    dependencies = "nvim-treesitter-textobjects",
+    opts = function()
+      local ai = require("mini.ai")
+      return {
+        silent = true,
+        n_lines = 100,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }, {}),
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+          g = function()
+            local eol = {
+              line = vim.fn.line("$"),
+              col = math.max(vim.fn.getline("$"):len(), 1),
+            }
+            return { from = { line = 1, col = 1 }, to = eol }
+          end,
+        },
+      }
+    end,
+    config = function(_, opts)
+      require("mini.ai").setup(opts)
+    end,
+  },
 }
