065d8070fbbeaaee709905600bc0c3c094263aa7
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2023-05-02 15:53:03 +0530

    fix: disable statuscol in specific modes

--- lua/core/extension/statuscolumn.lua
+++ lua/core/extension/statuscolumn.lua
@@ -5,7 +5,7 @@ api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   group = api.nvim_create_augroup("_StatusCol", { clear = true }),
   callback = function()
     local o = vim.opt_local
-    if o.nu:get() then
+    if not vim.tbl_contains(api.nvim_get_mode(), { "t", "nt", "ntT", "r?", "!" }) and o.nu:get() then
       o.statuscolumn = "%s%=%{v:relnum?v:relnum:v:lnum} %#StatusColSep#▐%#Normal#  "
     else
       o.statuscolumn = ""
