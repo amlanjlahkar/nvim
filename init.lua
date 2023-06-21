dae9930ff8354a0981ba583ee5c10a98d9801cca
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2023-06-21 13:55:01 +0530

    refactor(utils.req): use builtin lua bindings
    replace scan_dir() wrapper from plenary with builtin libuv binds for
    filesystem operations

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
 vim.loader.enable()
 vim.g.mapleader = " "
 
-require("lazyconf"):setup()
-require("color"):try_colorscheme()
 local req = require("core.utils.req")
 for _, m in pairs(req("core", req("core/utils"))) do
     require(m)
 end
+
+require("lazyconf"):setup()
+require("color"):try_colorscheme()
--- lua/core/utils/req.lua
+++ lua/core/utils/req.lua
@@ -1,47 +1,62 @@
 local fn = vim.fn
+local uv = vim.loop
 local fnmod = fn.fnamemodify
+
+local function pathmod(root_path, file_path)
+    local child = file_path:gsub(string.format("^%s/", root_path), ""):gsub("%/init.lua", "")
+    if child == "init.lua" then
+        return fnmod(root_path, ":t")
+    end
+    return child:find("init") and fnmod(child, ":h"):gsub("/", ".") or fnmod(child, ":r"):gsub("/", ".")
+end
 
 local M = {}
 ---Recursively search for files under path
 ---@param path string? Namespace path(defaults to current file's parent path)
 ---@param exclude table|function? Modules to exclude
----@param depth integer? Maximum depth to scan for nested modules
 ---@return table # Formatted file paths
-function M.req(path, exclude, depth)
-    local is_avail, plenary = pcall(require, "plenary")
-    if not is_avail then
-        error("Couldn't load plenary")
-    end
-    path = path and fn.stdpath("config") .. "/lua/" .. path or fn.expand("%:h")
+function M.req(path, exclude)
+    local apath = path and fn.stdpath("config") .. "/lua/" .. path or fn.expand("%:h")
+
     if exclude and type(exclude) == "table" then
         table.insert(exclude, "init")
     else
         exclude = { "init" }
     end
-    local modname = function(m)
-        local child = m:gsub(string.format("^%s/", path), "")
-        if child == "init.lua" then
-            return fnmod(path, ":t")
-        end
-        return child:find("init") and fnmod(child, ":h"):gsub("/", ".") or fnmod(child, ":r"):gsub("/", ".")
-    end
-    local mods = {}
-    plenary["scandir"].scan_dir(path, {
-        depth = depth,
-        on_insert = function(m)
-            if fnmod(m, ":e") ~= "lua" then
-                return
-            elseif not vim.tbl_contains(exclude, modname(m)) then
-                local root = fnmod(path, ":t")
-                if modname(m) == root then
-                    table.insert(mods, modname(m))
+
+    local submodules = {}
+    local subdir = vim.tbl_flatten({ apath })
+
+    repeat
+        local current_dir = table.remove(subdir, 1)
+        local fd = assert(uv.fs_scandir(current_dir))
+        if fd then
+            while true do
+                local fname, ftype = uv.fs_scandir_next(fd)
+                if fname == nil then
+                    break
+                elseif ftype == "directory" then
+                    table.insert(subdir, apath .. "/" .. fname)
                 else
-                    table.insert(mods, string.format("%s.%s", root, modname(m)))
+                    local fpath = vim.fs.find(fname, { path = apath })[1]
+                    local module = pathmod(apath, fpath)
+                    if fnmod(fpath, ":e") ~= "lua" then
+                        goto continue
+                    elseif not vim.tbl_contains(exclude, module) then
+                        local root = fnmod(apath, ":t")
+                        if module == root then
+                            table.insert(submodules, module)
+                        else
+                            table.insert(submodules, string.format("%s.%s", root, module))
+                        end
+                    end
                 end
+                ::continue::
             end
-        end,
-    })
-    return mods
+        end
+    until #subdir == 0
+
+    return submodules
 end
 
 return M.req
