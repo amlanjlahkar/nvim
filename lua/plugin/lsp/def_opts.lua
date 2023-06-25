f5912f829231a53efa27b88866c0f910991dc425
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2023-06-25 22:07:44 +0530

    chore: format

--- lua/plugin/lsp/def_opts.lua
+++ lua/plugin/lsp/def_opts.lua
@@ -42,58 +42,40 @@ function M.setup_keymaps(bufnr)
     local lsp = vim.lsp.buf
     local key = require("core.utils.map")
     local cmd, opts = key.cmd, key.new_opts
-  --stylua: ignore start
-  key.imap({ "<C-h>", lsp.signature_help })
-  key.nmap({
-    { "K",            lsp.hover,                                          opts(bufnr, "LSP: Show hover information") },
-    { "<leader>lr",   lsp.rename,                                         opts(bufnr, "LSP: Rename symbol under cursor") },
-    { "<leader>la",   lsp.code_action,                                    opts(bufnr, "LSP: List available code actions") },
-    { "<leader>lh",   lsp.signature_help,                                 opts(bufnr, "LSP: Show signature info for symbol under cursor") },
-    { "<leader>lt",   lsp.type_definition,                                opts(bufnr, "LSP: Goto type definition for symbol under cursor") },
-    { "<leader>lf",   vim.diagnostic.open_float,                          opts(bufnr, "LSP: Show line diagnostic") },
-    { "]d",           vim.diagnostic.goto_next,                           opts(bufnr, "LSP: Goto next diagnostic occurrence") },
-    { "[d",           vim.diagnostic.goto_prev,                           opts(bufnr, "LSP: Goto previous diagnostic occurrence") },
-    {
-      "gd", function()
-        check("definitions", "definition")
-      end, opts(bufnr, "LSP/Telescope: Goto definition")
-    },
-    {
-      "gi", function()
-        check("implementations", "implementation")
-      end, opts(bufnr, "LSP/Telescope: Goto implementation(s)")
-    },
-    {
-      "gr", function()
-        check("references")
-      end, opts(bufnr, "LSP/Telescope: List references")
-    },
-    {
-      "<leader>ls", function()
-        check("dynamic_workspace_symbols", "workspace_symbol", { fname_width = 40 })
-      end, opts(bufnr, "LSP/Telescope: Search for workspace symbols")
-    },
-  })
-  key.nxmap({
-    "<C-s>",
-    function()
-      vim.lsp.buf.format({
-        filter = function(client)
-          local use_builtin = { "clangd", "jdtls" }
-          for _, v in pairs(use_builtin) do
-            if client.name == v then
-              return client.name ~= "null-ls"
-            end
-          end
-          return client.name == "null-ls"
-        end,
-        timeout_ms = 5000,
-        async = true,
-      })
-    end,
-    opts(bufnr, "Lsp/Null-ls: Format buffer"),
-  })
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+    --stylua: ignore start
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+    key.imap({ "<C-h>", lsp.signature_help })
+    key.nmap({
+        { "K", lsp.hover, opts(bufnr) },
+        { "<leader>lr", lsp.rename, opts(bufnr) },
+        { "<leader>la", lsp.code_action, opts(bufnr) },
+        { "<leader>lh", lsp.signature_help, opts(bufnr) },
+        { "<leader>lt", lsp.type_definition, opts(bufnr) },
+        { "<leader>lf", vim.diagnostic.open_float, opts(bufnr) },
+        { "]d", vim.diagnostic.goto_next, opts(bufnr) },
+        { "[d", vim.diagnostic.goto_prev, opts(bufnr) },
+        { "gd", function() check("definitions", "definition") end, opts(bufnr) },
+        { "gi", function() check("implementations", "implementation") end, opts(bufnr) },
+        { "gr", function() check("references") end, opts(bufnr) },
+        { "<leader>ls", function() check("dynamic_workspace_symbols", "workspace_symbol", { fname_width = 40 }) end, opts(bufnr) },
+    })
+    key.nxmap({ "<C-s>", function()
+        vim.lsp.buf.format({
+            filter = function(client)
+                local use_builtin = { "clangd", "jdtls" }
+                for _, v in pairs(use_builtin) do
+                    if client.name == v then
+                        return client.name ~= "null-ls"
+                    end
+                end
+                return client.name == "null-ls"
+            end,
+            timeout_ms = 5000,
+            async = true,
+        })
+        end, opts(bufnr),
+    })
     --stylua: ignore end
 end
 -- 2}}}
@@ -126,6 +108,7 @@ function M.on_attach(client, bufnr)
     local diagnostics = require("plugin.lsp.diagnostics")
     diagnostics:setup_signs()
     vim.diagnostic.config(diagnostics:default_opts())
+
     M.setup_keymaps(bufnr)
 end
 -- 1}}}
