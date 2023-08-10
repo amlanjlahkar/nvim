4a314ff63c029cbdc57b26a0d1f2c8b2701aac34
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2023-08-10 21:55:09 +0530

    lsp.pkgs: disable unsued servers

--- lua/plugin/lsp/pkg_spec.lua
+++ lua/plugin/lsp/pkg_spec.lua
@@ -1,13 +1,13 @@
 return {
     "clangd",
     "efm",
-    { "cssls", mason_id = "css-lsp" },
-    { "html", mason_id = "html-lsp" },
+    { "cssls", mason_id = "css-lsp", hook_lspconfig = false },
+    { "html", mason_id = "html-lsp", hook_lspconfig = false },
     { "lua_ls", mason_id = "lua-language-server" },
     { "prettier", hook_lspconfig = false },
     { "rust_analyzer", auto_install = false, hook_lspconfig = false },
     { "shellcheck", hook_lspconfig = false },
     { "shfmt", hook_lspconfig = false },
     { "stylua", auto_install = false, hook_lspconfig = false },
-    { "tsserver", mason_id = "typescript-language-server", hook_lspconfig = true },
+    { "tsserver", mason_id = "typescript-language-server", hook_lspconfig = false },
 }
