ed7e10d9af10383f49d101848a5e9f5c7232524c
amlanjlahkar <amlanjlahkar@protonmail.com>
2024-01-29 23:15:35 +0530

    feat(color): color grouping

--- lua/color/custom/boo.lua
+++ lua/color/custom/boo.lua
@@ -1,62 +1,85 @@
+--[[
+Background
+    Shades:
+        - Lighter: default bg for emphasizing elements on floating windows
+        - Light: reserved(Cursorline)
+        - Dark: default bg for floating windows
+]]
 local Bg = "#0f0f10"
-local BgShadeLight = "#201c1c"
 local BgShadeLighter = "#211c1d"
-local Black = "#000000"
-local BlackShadeLight = "#131314"
-local Blue = "#a9d1df"
-local BlueShadeDark = "#849da2"
-local Fg = "#726464"
-local FgShadeDarker = "#51494a"
-local Green = "#63b0b0"
-local Magenta = "#d5a8e3"
-local Red = "#d19299"
-local RedShadeDarker = "#473234"
-local RedShadeLight = "#81363f"
-local RedShadeLighter = "#e4627e"
-local White = "#f3dadc"
-local none = "NONE"
+local BgShadeLight = "#201c1c"
+local BgShadeDark = "#141415"
 
+--[[
+Foreground
+    Shades:
+        - Light: Default fg for important elements
+        - Dark: Default fg for less important elements
+]]
+local Fg = "#726464"
+local FgShadeLight = "#a99697"
+local FgShadeDark = "#51494a"
+
+--[[
+Indicator
+Red (default fg for selections/matches/warnings etc.)
+    Shades:
+        - Light: Foreground complement to normal Red
+        - Dark: More distinct alternative to FgShadeDark
+]]
+local Red = "#d19299"
+local RedShadeLight = "#e4627e"
+local RedShadeDark = "#473234"
+
+-- Misc
+local Green = "#63b0b0"
+local Blue = "#a9d1df"
+local Magenta = "#d5a8e3"
+local Cyan = "#849da2"
+
+-- Diffs
 local DiffAddBg = "#0c1d0e"
-local DiffDeleteBg = "#2a080f"
 local DiffAddFg = "#2f7373"
+local DiffDeleteBg = "#2a080f"
 local DiffDeleteFg = "#a6334c"
+
+local none = "NONE"
 
 local M = {}
 --stylua: ignore
 M.custom = {
   -- statusline {{{1
-  { "StatusLine",                 { bg = BlackShadeLight, fg = FgShadeDarker } },
-  { "StatusLineDiagnosticError",  { bg = BlackShadeLight, fg = "#fe697d" } },
-  { "StatusLineDiagnosticHint",   { bg = BlackShadeLight, fg = "#694347" } },
-  { "StatusLineDiagnosticInfo",   { bg = BlackShadeLight, fg = "#e4dcec" } },
-  { "StatusLineDiagnosticWarn",   { bg = BlackShadeLight, fg = "#dfb8bc" } },
-  { "StatusLineImp",              { bg = BlackShadeLight, fg = Fg } },
-  { "StatusLineInd",              { bg = BlackShadeLight, fg = Red } },
-  { "StatusLineNC",               { bg = BlackShadeLight, fg = Fg } },
+  { "StatusLine",                 { bg = BgShadeDark, fg = FgShadeDark } },
+  { "StatusLineDiagnosticError",  { bg = BgShadeDark, fg = "#fe697d" } },
+  { "StatusLineDiagnosticHint",   { bg = BgShadeDark, fg = "#694347" } },
+  { "StatusLineDiagnosticInfo",   { bg = BgShadeDark, fg = "#e4dcec" } },
+  { "StatusLineDiagnosticWarn",   { bg = BgShadeDark, fg = "#dfb8bc" } },
+  { "StatusLineImp",              { bg = BgShadeDark, fg = FgShadeLight } },
+  { "StatusLineInd",              { bg = BgShadeDark, fg = Red } },
+  { "StatusLineNC",               { bg = BgShadeDark, fg = Fg } },
   -- 1}}}
   -- native {{{1
-  { "LineNr",                     { bg = Bg, fg = RedShadeDarker } },
-  { "CursorLineNr",               { bg = Bg, fg = White, bold = false } },
+  { "LineNr",                     { bg = Bg, fg = RedShadeDark } },
+  { "CursorLineNr",               { bg = Bg, fg = FgShadeLight, bold = false } },
   { "SignColumn",                 { bg = Bg } },
-  { "NormalFloat",                { bg = Bg, fg = Fg } },
-  { "FloatBorder",                { bg = Bg, fg = BgShadeLighter } },
-  { "FloatTitle",                 { bg = BlackShadeLight, fg = Fg, underline = true } },
-  { "Pmenu",                      { bg = Black , fg = Fg } },
-  { "PmenuSbar",                  { bg = BlackShadeLight  } },
+  { "NormalFloat",                { bg = BgShadeDark, fg = Fg } },
+  { "FloatBorder",                { bg = BgShadeDark, fg = BgShadeDark } },
+  { "FloatTitle",                 { bg = BgShadeLighter, fg = Fg, underline = true } },
+  { "Pmenu",                      { bg = BgShadeDark , fg = Fg } },
+  { "PmenuSbar",                  { bg = BgShadeLighter  } },
   { "PmenuSel",                   { bg = BgShadeLighter, fg = Red } },
-  { "PmenuThumb",                 { bg = BgShadeLighter } },
-  { "TabLine",                    { bg = BlackShadeLight, fg = FgShadeDarker } },
-  { "TabLineFill",                { bg = BlackShadeLight } },
-  { "TabLineSel",                 { bg = BgShadeLight, fg = Fg, bold = false } },
-  { "VertSplit",                  { fg = BgShadeLighter } },
-  { "Visual",                     { bg = BgShadeLighter, fg = White } },
+  { "PmenuThumb",                 { bg = RedShadeDark } },
+  { "TabLine",                    { bg = BgShadeDark, fg = FgShadeDark, bold = true } },
+  { "TabLineFill",                { bg = BgShadeDark } },
+  { "TabLineSel",                 { bg = Bg, fg = FgShadeLight, bold = true } },
+  { "Visual",                     { bg = BgShadeLighter, fg = FgShadeLight } },
   { "MatchParen",                 { bg = "#351a3d", fg = Magenta, bold = true } },
   { "QuickFixLine",               { bg = BgShadeLighter } },
-  { "NonText",                    { fg = FgShadeDarker } },
+  { "NonText",                    { fg = FgShadeDark } },
   { "Comment",                    { bg = none, fg = "#9a666d", italic = true } },
   { "Error",                      { bg = none, fg = none } }, -- causes weird paren,brace highlighting on floating windows by default
   -- health {{{2
-  { "healthError",                { fg = RedShadeLighter } },
+  { "healthError",                { fg = RedShadeLight } },
   { "healthSuccess",              { fg = Green } },
   { "healthWarning",              { fg = RedShadeLight } },
   -- 2}}}
@@ -94,13 +117,13 @@ M.custom = {
   -- 1}}}
   -- plugins {{{1
   -- cmp {{{2
-  { "CmpDocBorder",               { bg = BlackShadeLight, fg = BlackShadeLight } },
-  { "CmpDocNormal",               { bg = BlackShadeLight, fg = Fg } },
-  { "CmpItemKind",                { fg = BlueShadeDark } },
-  { "CmpItemMenu",                { fg = FgShadeDarker } },
+  { "CmpDocBorder",               { bg = BgShadeLighter, fg = BgShadeLighter } },
+  { "CmpDocNormal",               { bg = BgShadeLighter, fg = Fg } },
+  { "CmpItemKind",                { fg = Cyan } },
+  { "CmpItemMenu",                { fg = FgShadeDark } },
   { "CmpItemKindCopilot",         { fg = Magenta } },
-  { "FloatBorderAlt",             { bg = Black, fg = Black } },
-  { "NormalFloatAlt",             { bg = Black, fg = Fg } },
+  { "FloatBorderAlt",             { bg = BgShadeDark, fg = BgShadeDark } },
+  { "NormalFloatAlt",             { bg = BgShadeDark, fg = Fg } },
   -- 2}}}
   -- gitsigns/fugitive {{{2
   { "DiffAdd",                    { bg = DiffAddBg, fg = DiffAddFg } },
@@ -110,35 +133,36 @@ M.custom = {
   { "GitGutterChangeDelete",      { bg = Bg, fg = Magenta } },
   { "GitGutterDelete",            { bg = Bg, fg = Red } },
   { "GitSignsAddInline",          { bg = DiffAddBg, fg = Green, bold = true } },
-  { "GitSignsDeleteInline",       { bg = DiffDeleteBg, fg = RedShadeLighter, bold = true } },
+  { "GitSignsDeleteInline",       { bg = DiffDeleteBg, fg = RedShadeLight, bold = true } },
   { "diffAdded",                  { bg = DiffAddBg, fg = Green } },
-  { "diffRemoved",                { bg = DiffDeleteBg, fg = RedShadeLighter } },
+  { "diffRemoved",                { bg = DiffDeleteBg, fg = RedShadeLight } },
   -- 2}}}
   -- lazy {{{2
-  { "LazyProgressTodo",           { fg = Black } },
+  { "LazyProgressTodo",           { fg = BgShadeDark } },
   -- 2}}}
   -- mason {{{2
-  { "MasonHeader",                { bg = White, fg = Black } },
-  { "MasonHighlightBlockBold",    { bg = BgShadeLighter, fg = White } },
+  { "MasonHeader",                { bg = FgShadeLight, fg = BgShadeLighter } },
+  { "MasonHighlightBlockBold",    { bg = BgShadeLighter, fg = FgShadeLight } },
   { "MasonMutedBlock",            { bg = BgShadeLighter, fg = Fg } },
   -- 2}}}
   -- telescope {{{2
-  { "TelescopeBorder",            { bg = Bg, fg = BgShadeLighter } },
-  { "TelescopeMatching",          { fg = RedShadeLighter } },
-  { "TelescopeNormal",            { bg = Bg, fg = Fg } },
-  { "TelescopePromptCounter",     { fg = FgShadeDarker } },
-  { "TelescopePromptTitle",       { bg = BlackShadeLight, fg = Fg } },
+  { "TelescopeBorder",            { bg = BgShadeDark, fg = BgShadeDark } },
+  { "TelescopeMatching",          { fg = RedShadeLight } },
+  { "TelescopeNormal",            { bg = BgShadeDark, fg = Fg } },
+  { "TelescopePromptCounter",     { fg = FgShadeDark } },
+  { "TelescopePromptTitle",       { bg = BgShadeLighter, fg = Fg } },
   { "TelescopeSelection",         { bg = BgShadeLighter, fg = Red } },
   -- 2}}}
   -- other {{{2
   { "FidgetTitle",                { fg = Magenta, blend = 0 } },
-  { "FidgetTask",                 { fg = FgShadeDarker, blend = 0 } },
+  { "FidgetTask",                 { fg = FgShadeDark, blend = 0 } },
   { "LspReference",               { bg = BgShadeLighter, fg = "#d9d6cf", underline = false } },
-  { "MiniIndentScopeSymbol",      { fg = RedShadeDarker } },
+  { "MiniIndentScopeSymbol",      { fg = RedShadeDark } },
   { "SnipChoiceNode",             { bg = BgShadeLight, fg = Blue } },
   { "SnipInsertNode",             { bg = BgShadeLight, fg = Green } },
-  { "StatusColSep",               { bg = BlackShadeLight, fg = BgShadeLighter } },
-  { "TreesitterContext",          { bg = BlackShadeLight, italic = false } },
+  { "StatusColSep",               { bg = BgShadeDark, fg = BgShadeLighter } },
+  { "TreesitterContext",          { bg = BgShadeDark, italic = false } },
+
   -- 2}}}
   -- 1}}}
 }
