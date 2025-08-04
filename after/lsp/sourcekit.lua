a84f9d0bbc913d8bf7005f28fd30c51b20b50014
Amlanjyoti Lahkar <amlanjlahkar@protonmail.com>
2025-08-05 00:25:49 +0530

    Add typescript and sourcekit lsp config

---
+++ after/lsp/sourcekit.lua
@@ -0,0 +1,18 @@
+return {
+    cmd = { 'sourcekit-lsp' },
+    filetypes = { 'swift' },
+    root_markers = { 'Package.swift' },
+    capabilities = {
+        workspace = {
+            didChangeWatchedFiles = {
+                dynamicRegistration = true,
+            },
+        },
+        textDocument = {
+            diagnostic = {
+                dynamicRegistration = true,
+                relatedDocumentSupport = true,
+            },
+        },
+    },
+}
