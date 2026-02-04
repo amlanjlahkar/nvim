20c616180f9c122a209e6a010f55f0cf14817480
Amlanjyoti Lahkar <amlanjlahkar@protonmail.com>
2026-02-04 20:40:35 +0530

    Update hl group

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,11 +13,15 @@ return {
 
             -- Highlight group overrides
             set_hl(0, 'WinSeparator', { fg = '#222827' })
+
             set_hl(0, 'StatusLine', { fg = '#63b0b0', bg = '#0d0d0d' })
             set_hl(0, 'StatusLineNC', { fg = '#3a3a3a', bg = '#0d0d0d' })
+
             set_hl(0, 'TabLine', { fg = '#3a3a3a', bg = '#0d0d0d' })
             set_hl(0, 'TabLineFill', { fg = '#3a3a3a', bg = '#0d0d0d' })
             set_hl(0, 'TabLineSel', { fg = '#9c75dd', bg = '#0d0d0d' })
+
+            set_hl(0, 'DiagnosticError', { fg = '#7f5479' })
         end,
     },
 }
