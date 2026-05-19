b3312dac0a2610760405219cbb604f4aacfe9db0
Amlanjyoti Lahkar <amlanjlahkar@protonmail.com>
2026-05-19 22:27:06 +0530

    Add plugin install/update hooks for mason and treesitter

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -1,3 +1,20 @@
+vim.api.nvim_create_autocmd('PackChanged', {
+    once = true,
+    callback = function(ev)
+        local name, kind = ev.data.spec.name, ev.data.kind
+        if name == 'mason.nvim' and kind == 'install' then
+            if not ev.data.active then
+                vim.cmd.packadd('mason.nvim')
+            end
+            -- Defer calling user command to prevent installation
+            -- attempt of packages before loading the 'mason' module
+            vim.defer_fn(function()
+                vim.cmd('MasonEnsureInstalled')
+            end, 2500)
+        end
+    end,
+})
+
 vim.pack.add({
     'https://github.com/mason-org/mason.nvim',
 })
--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,9 +1,21 @@
+local api = vim.api
+local au = api.nvim_create_autocmd
+
+api.nvim_create_autocmd('PackChanged', {
+    callback = function(ev)
+        local name, kind = ev.data.spec.name, ev.data.kind
+        if name == 'nvim-treesitter' and kind == 'update' then
+            if not ev.data.active then
+                vim.cmd.packadd('nvim-treesitter')
+            end
+            vim.cmd('TSUpdate')
+        end
+    end,
+})
+
 vim.pack.add({
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
 })
-
-local api = vim.api
-local au = api.nvim_create_autocmd
 
 local parsers = {
     'bash',
