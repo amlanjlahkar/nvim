04cf5903c3ee76910d44c8e023d0e48fdfb53da1
Amlanjyoti Lahkar <amlanjlahkar@protonmail.com>
2026-05-21 00:32:28 +0530

    Removed plugins folder used by lazy

--- lua/plugins/fidget.lua
+++
@@ -1,6 +0,0 @@
-return {
-    'j-hui/fidget.nvim',
-    event = 'LspAttach',
-    -- pin = true;
-    opts = {},
-}
--- lua/plugins/minisurround.lua
+++
@@ -1,6 +0,0 @@
-return {
-    'nvim-mini/mini.surround',
-    version = false,
-    keys = { 's', { 's', mode = 'x' } },
-    opts = {},
-}
