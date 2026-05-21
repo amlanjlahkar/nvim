0af6598d7f8a8c661a4bb4f2fc32c99b5e6ee8e1
Amlanjyoti Lahkar <amlanjlahkar@protonmail.com>
2026-05-21 21:10:29 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Enable experimental lua module loader from lazy
 vim.loader.enable()
 
 vim.g.mapleader = ' '
@@ -9,6 +10,37 @@ require('config.usercmds')
 require('config.autocmds')
 require('config.lsp')
 
+-- Retrieve directories relative to cwd
+-- to be set as the value of 'path'
+local find_cmd = {
+    'fd',
+    '--no-follow',
+    '--hidden',
+    '--exclude=.git',
+    '--exclude=.jj',
+    '--no-require-git',
+    '--color=never',
+    '--type=d',
+}
+
+local handle = {}
+
+local on_exit = function(obj)
+    assert(obj.code, obj.stderr)
+
+    local path = string.gsub(handle._state.stdout_data[1], '\n', ',')
+
+    vim.schedule(function()
+        vim.opt.path = '.,' .. path
+    end)
+end
+
+handle = vim.system(find_cmd, {
+    cwd = vim.uv.cwd(),
+    text = true,
+}, on_exit)
+
+-- :help ui2
 require('vim._core.ui2').enable({})
 
 vim.cmd.packadd('nvim.undotree')
